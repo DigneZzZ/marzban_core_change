f9c9e79a40dea79460fcd8890288e38fc049aade
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2024-02-26 19:09:10 +0300

    Update change.sh

--- change.sh
+++ change.sh
@@ -1,115 +1,222 @@
 #!/bin/bash
-set -x
-# Функция для выбора версии Xray-core
-select_xray_version() {
-    local latest_releases=$(curl -s "https://api.github.com/repos/XTLS/Xray-core/releases?per_page=4")
-    local versions=($(echo "$latest_releases" | grep -oP '"tag_name": "\K(.*?)(?=")'))
-    echo "Доступные версии Xray-core:"
-    for ((i=0; i<${#versions[@]}; i++)); do
-        echo "$(($i + 1)): ${versions[i]}"
-    done
-    printf "Выберите версию для установки (1-${#versions[@]}), или нажмите Enter для отказа: "
-    read choice
-    if [ -z "$choice" ]; then
-        echo "Установка ядра отменена."
-        exit 1
-    fi
-    choice=$((choice - 1))
-    if [ "$choice" -lt 0 ] || [ "$choice" -ge "${#versions[@]}" ]; then
-        echo "Неверный выбор. Установка ядра отменена."
-        exit 1
-    fi
-    selected_version=${versions[choice]}
-    echo "Выбрана версия $selected_version для установки."
-    echo "$selected_version"
-}
+# Вывод заголовка
+echo '  
+ █████ █████                                                                                    █████                                                      
+░░███ ░░███                                                                                    ░░███                                                       
+ ░░███ ███   ████████   ██████   █████ ████     ██████   ██████  ████████   ██████      ██████  ░███████    ██████   ████████    ███████  ██████  ████████ 
+  ░░█████   ░░███░░███ ░░░░░███ ░░███ ░███     ███░░███ ███░░███░░███░░███ ███░░███    ███░░███ ░███░░███  ░░░░░███ ░░███░░███  ███░░███ ███░░███░░███░░███
+   ███░███   ░███ ░░░   ███████  ░███ ░███    ░███ ░░░ ░███ ░███ ░███ ░░░ ░███████    ░███ ░░░  ░███ ░███   ███████  ░███ ░███ ░███ ░███░███████  ░███ ░░░ 
+  ███ ░░███  ░███      ███░░███  ░███ ░███    ░███  ███░███ ░███ ░███     ░███░░░     ░███  ███ ░███ ░███  ███░░███  ░███ ░███ ░███ ░███░███░░░   ░███     
+ █████ █████ █████    ░░████████ ░░███████    ░░██████ ░░██████  █████    ░░██████    ░░██████  ████ █████░░████████ ████ █████░░███████░░██████  █████    
+░░░░░ ░░░░░ ░░░░░      ░░░░░░░░   ░░░░░███     ░░░░░░   ░░░░░░  ░░░░░      ░░░░░░      ░░░░░░  ░░░░ ░░░░░  ░░░░░░░░ ░░░░ ░░░░░  ░░░░░███ ░░░░░░  ░░░░░     
+                                  ███ ░███                                                                                      ███ ░███                   
+                                 ░░██████                                                                                      ░░██████                    
+                                  ░░░░░░                                                                                        ░░░░░░                     
+                                  
 
-# Функция для поиска пути до папки Marzban-node и файла docker-compose.yml
-find_marzban_node_dir() {
-    local marzban_node_dir=""
-    marzban_node_dir=$(find / -type d -name "Marzban-node" -exec test -f "{}/docker-compose.yml" \; -print -quit)
-    echo "$marzban_node_dir"
-}
+ ███████████                       ██████   ██████                                █████                         
+░░███░░░░░░█                      ░░██████ ██████                                ░░███                          
+ ░███   █ ░   ██████  ████████     ░███░█████░███   ██████   ████████   █████████ ░███████   ██████   ████████  
+ ░███████    ███░░███░░███░░███    ░███░░███ ░███  ░░░░░███ ░░███░░███ ░█░░░░███  ░███░░███ ░░░░░███ ░░███░░███ 
+ ░███░░░█   ░███ ░███ ░███ ░░░     ░███ ░░░  ░███   ███████  ░███ ░░░  ░   ███░   ░███ ░███  ███████  ░███ ░███ 
+ ░███  ░    ░███ ░███ ░███         ░███      ░███  ███░░███  ░███        ███░   █ ░███ ░███ ███░░███  ░███ ░███ 
+ █████      ░░██████  █████        █████     █████░░████████ █████      █████████ ████████ ░░████████ ████ █████
+░░░░░        ░░░░░░  ░░░░░        ░░░░░     ░░░░░  ░░░░░░░░ ░░░░░      ░░░░░░░░░ ░░░░░░░░   ░░░░░░░░ ░░░░ ░░░░░ 
+                                                                                                                
 
-# Функция для изменения ядра Marzban в папке Marzban-node
-change_marzban_node_core() {
-    local marzban_node_dir="$1"
-    local xray_executable_path='XRAY_EXECUTABLE_PATH="/var/lib/marzban/xray-core/xray"'
-    if ! grep -q "XRAY_EXECUTABLE_PATH: \"/var/lib/marzban/xray-core/xray\"" "$marzban_node_dir/docker-compose.yml"; then
-        sed -i '/environment:/!b;n;/XRAY_EXECUTABLE_PATH/!a\      XRAY_EXECUTABLE_PATH: "/var/lib/marzban/xray-core/xray"' "$marzban_node_dir/docker-compose.yml"
-    fi
-    echo "Перезапуск Marzban-node..."
-    cd "$marzban_node_dir" || exit
-    docker compose up -d --force-recreate
-}
+ █████                   ██████████    ███                               ███████████            ███████████
+░░███                   ░░███░░░░███  ░░░                               ░█░░░░░░███            ░█░░░░░░███ 
+ ░███████  █████ ████    ░███   ░░███ ████   ███████ ████████    ██████ ░     ███░    █████████░     ███░  
+ ░███░░███░░███ ░███     ░███    ░███░░███  ███░░███░░███░░███  ███░░███     ███     ░█░░░░███      ███    
+ ░███ ░███ ░███ ░███     ░███    ░███ ░███ ░███ ░███ ░███ ░███ ░███████     ███      ░   ███░      ███     
+ ░███ ░███ ░███ ░███     ░███    ███  ░███ ░███ ░███ ░███ ░███ ░███░░░    ████     █   ███░   █  ████     █
+ ████████  ░░███████     ██████████   █████░░███████ ████ █████░░██████  ███████████  █████████ ███████████
+░░░░░░░░    ░░░░░███    ░░░░░░░░░░   ░░░░░  ░░░░░███░░░░ ░░░░░  ░░░░░░  ░░░░░░░░░░░  ░░░░░░░░░ ░░░░░░░░░░░ 
+            ███ ░███                        ███ ░███                                                       
+           ░░██████                        ░░██████                                                        
+            ░░░░░░                          ░░░░░░                                                         
+'
+sleep 2s
+echo -e "\e[1m\e[33mДанный скрипт устанавливает ядро Xray в Marzban и Marzban Node\n\e[0m"
+sleep 1
 
-# Функция для изменения ядра Marzban в папке marzban_folder
-change_marzban_core() {
-    local marzban_env_file="$1/.env"
-    local xray_executable_path='XRAY_EXECUTABLE_PATH="/var/lib/marzban/xray-core/xray"'
-    if ! grep -q "^${xray_executable_path}" "$marzban_env_file"; then
-        echo "${xray_executable_path}" >> "${marzban_env_file}"
-    fi
-    echo "Перезапуск Marzban..."
-    marzban restart -n
-}
+# Функция для обновления ядра Marzban Main
+update_marzban_main() {
+# Отправляем запрос к GitHub API для получения информации о последних четырех релизах
+latest_releases=$(curl -s "https://api.github.com/repos/XTLS/Xray-core/releases?per_page=4")
 
-# Функция для установки Xray-core
-install_xray_core() {
-    local selected_version="$1"
-    # Проверяем, установлены ли необходимые пакеты
-    if ! dpkg -s wget unzip >/dev/null 2>&1; then
-        echo "Установка необходимых пакетов..."
-        apt install -y wget unzip
-    fi
-    # Создаем папку /var/lib/marzban/xray-core
-    mkdir -p /var/lib/marzban/xray-core
-    # Переходим в папку /var/lib/marzban/xray-core
-    cd /var/lib/marzban/xray-core || exit
-    # Скачиваем Xray-core выбранной версии
-    local xray_filename="Xray-linux-64.zip"
-    local xray_download_url="https://github.com/XTLS/Xray-core/releases/download/${selected_version}/${xray_filename}"
-    echo "Скачивание Xray-core версии ${selected_version}..."
-    wget "${xray_download_url}"
-    # Извлекаем файл из архива и удаляем архив
-    echo "Извлечение Xray-core..."
-    unzip "${xray_filename}"
-    rm "${xray_filename}"
-}
+# Извлекаем версии из JSON-ответа
+versions=($(echo "$latest_releases" | grep -oP '"tag_name": "\K(.*?)(?=")'))
 
-# Выбор версии Xray-core
-selected_version=$(select_xray_version)
+# Печатаем доступные версии
+echo "Доступные версии Xray-core:"
+for ((i=0; i<${#versions[@]}; i++)); do
+    echo "$(($i + 1)): ${versions[i]}"
+done
 
-# Если пользователь отказался от установки, выходим
-if [ -z "$selected_version" ]; then
-    exit 1
+# Предлагаем пользователю выбрать версию
+printf "Выберите версию для установки (1-${#versions[@]}), или нажмите Enter для выбора последней по умолчанию (${versions[0]}): "
+read choice
+
+# Проверяем, был ли сделан выбор пользователем
+if [ -z "$choice" ]; then
+    choice="1"  # Выбираем самую свежую версию по умолчанию
 fi
 
-# Путь до папки Marzban-node и файла docker-compose.yml по умолчанию
+# Преобразуем выбор пользователя в индекс массива
+choice=$((choice - 1))
+
+# Проверяем, что выбор пользователя в пределах доступных версий
+if [ "$choice" -lt 0 ] || [ "$choice" -ge "${#versions[@]}" ]; then
+    echo "Неверный выбор. Выбрана последняя версия по умолчанию (${versions[0]})."
+    choice=$((${#versions[@]} - 1))  # Выбираем последнюю версию по умолчанию
+fi
+
+# Выбираем версию Xray-core для установки
+selected_version=${versions[choice]}
+echo "Выбрана версия $selected_version для установки."
+
+# Проверяем, установлены ли необходимые пакеты
+if ! dpkg -s wget unzip >/dev/null 2>&1; then
+  echo "Установка необходимых пакетов..."
+  apt install -y wget unzip
+fi
+
+# Создаем папку /var/lib/marzban/xray-core
+mkdir -p /var/lib/marzban/xray-core
+# Переходим в папку /var/lib/marzban/xray-core
+cd /var/lib/marzban/xray-core
+
+# Скачиваем Xray-core выбранной версии
+xray_filename="Xray-linux-64.zip"
+xray_download_url="https://github.com/XTLS/Xray-core/releases/download/${selected_version}/${xray_filename}"
+
+echo "Скачивание Xray-core версии ${selected_version}..."
+wget "${xray_download_url}"
+
+# Извлекаем файл из архива и удаляем архив
+echo "Извлечение Xray-core..."
+unzip "${xray_filename}"
+rm "${xray_filename}"
+
+# Изменение ядра Marzban
 marzban_folder="/opt/marzban"
+marzban_env_file="${marzban_folder}/.env"
+xray_executable_path='XRAY_EXECUTABLE_PATH="/var/lib/marzban/xray-core/xray"'
 
-# Поиск пути до папки marzban_folder
-if [ -d "$marzban_folder" ]; then
-    marzban_node_dir="$marzban_folder"
-else
-    marzban_node_dir=$(find_marzban_node_dir)
+echo "Изменение ядра Marzban..."
+# Проверяем, существует ли уже строка XRAY_EXECUTABLE_PATH в файле .env
+if ! grep -q "^${xray_executable_path}" "$marzban_env_file"; then
+  # Если строка отсутствует, добавляем ее
+  echo "${xray_executable_path}" >> "${marzban_env_file}"
 fi
 
-# Если найдены обе папки, меняем ядро в обоих местах
-if [ -n "$marzban_folder" ] && [ -n "$marzban_node_dir" ]; then
-    change_marzban_node_core "$marzban_node_dir"
-    change_marzban_core "$marzban_folder"
-# Если найдена только папка marzban_folder, меняем ядро в ней
-elif [ -n "$marzban_folder" ]; then
-    change_marzban_core "$marzban_folder"
-# Если найдена только папка Marzban-node, меняем ядро в ней
-elif [ -n "$marzban_node_dir" ]; then
-    change_marzban_node_core "$marzban_node_dir"
-fi
-
-# Установка Xray-core выбранной версии
-install_xray_core "$selected_version"
-
-set +x
+# Перезапускаем Marzban
+echo "Перезапуск Marzban..."
+marzban restart -n
 
 echo "Установка завершена."
+}
+
+# Функция для обновления ядра Marzban Node
+update_marzban_node() {
+
+# Отправляем запрос к GitHub API для получения информации о последних четырех релизах
+latest_releases=$(curl -s "https://api.github.com/repos/XTLS/Xray-core/releases?per_page=4")
+
+# Извлекаем версии из JSON-ответа
+versions=($(echo "$latest_releases" | grep -oP '"tag_name": "\K(.*?)(?=")'))
+
+# Печатаем доступные версии
+echo "Доступные версии Xray-core:"
+for ((i=0; i<${#versions[@]}; i++)); do
+    echo "$(($i + 1)): ${versions[i]}"
+done
+
+# Предлагаем пользователю выбрать версию
+printf "Выберите версию для установки (1-${#versions[@]}), или нажмите Enter для выбора последней по умолчанию (${versions[0]}): "
+read choice
+
+# Проверяем, был ли сделан выбор пользователем
+if [ -z "$choice" ]; then
+    choice="1"  # Выбираем самую свежую версию по умолчанию
+fi
+
+# Преобразуем выбор пользователя в индекс массива
+choice=$((choice - 1))
+
+# Проверяем, что выбор пользователя в пределах доступных версий
+if [ "$choice" -lt 0 ] || [ "$choice" -ge "${#versions[@]}" ]; then
+    echo "Неверный выбор. Выбрана последняя версия по умолчанию (${versions[0]})."
+    choice=$((${#versions[@]} - 1))  # Выбираем последнюю версию по умолчанию
+fi
+
+# Выбираем версию Xray-core для установки
+selected_version=${versions[choice]}
+echo "Выбрана версия $selected_version для установки."
+
+# Проверяем, установлены ли необходимые пакеты
+if ! dpkg -s wget unzip >/dev/null 2>&1; then
+  echo "Установка необходимых пакетов..."
+  apt install -y wget unzip
+fi
+
+# Создаем папку /var/lib/marzban/xray-core
+mkdir -p /var/lib/marzban/xray-core
+# Переходим в папку /var/lib/marzban/xray-core
+cd /var/lib/marzban/xray-core
+
+# Скачиваем Xray-core выбранной версии
+xray_filename="Xray-linux-64.zip"
+xray_download_url="https://github.com/XTLS/Xray-core/releases/download/${selected_version}/${xray_filename}"
+
+echo "Скачивание Xray-core версии ${selected_version}..."
+wget "${xray_download_url}"
+
+# Извлекаем файл из архива и удаляем архив
+echo "Извлечение Xray-core..."
+unzip "${xray_filename}"
+rm "${xray_filename}"
+
+    # Поиск пути до папки Marzban-node и файла docker-compose.yml
+    marzban_node_dir=$(find / -type d -name "Marzban-node" -exec test -f "{}/docker-compose.yml" \; -print -quit)
+
+    if [ -z "$marzban_node_dir" ]; then
+        echo "Папка Marzban-node с файлом docker-compose.yml не найдена"
+        exit 1
+    fi
+
+    # Проверяем, существует ли уже строка XRAY_EXECUTABLE_PATH в файле docker-compose.yml
+    if ! grep -q "XRAY_EXECUTABLE_PATH: \"/var/lib/marzban/xray-core/xray\"" "$marzban_node_dir/docker-compose.yml"; then
+        # Если строка отсутствует, добавляем ее
+        sed -i '/environment:/!b;n;/XRAY_EXECUTABLE_PATH/!a\      XRAY_EXECUTABLE_PATH: "/var/lib/marzban/xray-core/xray"' "$marzban_node_dir/docker-compose.yml"
+    fi
+
+    # Перезапускаем Marzban-node
+    echo "Перезапуск Marzban..."
+    cd "$marzban_node_dir" || exit
+    docker compose up -d --force-recreate
+
+    echo "Обновление ядра на Marzban-node завершено. Ядро установлено версии $xray_version"
+}
+
+# Печатаем доступные опции для пользователя
+echo "Выберите Marzban, для которого необходимо обновить ядро:"
+echo "1. Marzban Main"
+echo "2. Marzban Node"
+
+# Запрос на выбор опции у пользователя
+read -p "Введите номер выбранной опции: " option
+
+# Проверяем выбор пользователя и вызываем соответствующую функцию
+case $option in
+    1)
+        update_marzban_main
+        ;;
+    2)
+        update_marzban_node
+        ;;
+    *)
+        echo "Неверный выбор. Выберите 1 или 2."
+        ;;
+esac
