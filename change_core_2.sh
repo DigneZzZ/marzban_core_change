816f80f089d680a468fde81427a5b9a173954b7a
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2024-02-26 17:54:53 +0300

    Create change_core_2.sh

---
+++ change_core_2.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Отправляем запрос к GitHub API для получения информации о последних трех релизах
+latest_releases=$(curl -s "https://api.github.com/repos/XTLS/Xray-core/releases?per_page=3")
+
+# Извлекаем версии из JSON-ответа
+versions=($(echo "$latest_releases" | grep -oP '"tag_name": "\K(.*?)(?=")'))
+
+# Печатаем доступные версии
+echo "Доступные версии Xray-core:"
+for ((i=0; i<${#versions[@]}; i++)); do
+    echo "$(($i + 1)): ${versions[i]}"
+done
+
+# Предлагаем пользователю выбрать версию
+read -p "Выберите версию для установки (1-${#versions[@]}): " choice
+choice=$((choice - 1))  # Преобразуем в индекс массива
+
+# Проверяем, что выбор пользователя в пределах доступных версий
+if [ "$choice" -lt 0 ] || [ "$choice" -ge "${#versions[@]}" ]; then
+    echo "Неверный выбор. Выбрана последняя версия по умолчанию."
+    choice=$((${#versions[@]} - 1))  # Выбираем последнюю версию по умолчанию
+fi
+
+# Выбираем версию Xray-core для установки
+selected_version=${versions[choice]}
+echo "Выбрана версия $selected_version для установки."
+
+# Проверяем, установлены ли необходимые пакеты
+if ! dpkg -s wget unzip >/dev/null 2>&1; then
+  echo "Установка необходимых пакетов..."
+  apt install -y wget unzip
+fi
+
+# Создаем папку /var/lib/marzban/xray-core
+mkdir -p /var/lib/marzban/xray-core
+# Переходим в папку /var/lib/marzban/xray-core
+cd /var/lib/marzban/xray-core
+
+# Скачиваем Xray-core выбранной версии
+xray_filename="Xray-linux-64.zip"
+xray_download_url="https://github.com/XTLS/Xray-core/releases/download/v${selected_version}/${xray_filename}"
+
+echo "Скачивание Xray-core версии ${selected_version}..."
+wget "${xray_download_url}"
+
+# Извлекаем файл из архива и удаляем архив
+echo "Извлечение Xray-core..."
+unzip "${xray_filename}"
+rm "${xray_filename}"
+
+# Изменение ядра Marzban
+marzban_folder="/opt/marzban"
+marzban_env_file="${marzban_folder}/.env"
+xray_executable_path='XRAY_EXECUTABLE_PATH="/var/lib/marzban/xray-core/xray"'
+
+echo "Изменение ядра Marzban..."
+# Проверяем, существует ли уже строка XRAY_EXECUTABLE_PATH в файле .env
+if ! grep -q "^${xray_executable_path}" "$marzban_env_file"; then
+  # Если строка отсутствует, добавляем ее
+  echo "${xray_executable_path}" >> "${marzban_env_file}"
+fi
+
+# Перезапускаем Marzban
+echo "Перезапуск Marzban..."
+marzban restart -n
+
+echo "Установка завершена."
